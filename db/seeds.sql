d3170d0206c7fcf6a440275acd5b401cc8b70d0a
Sara Muschkin <sunshinesara55@MacBook-Pro-2.local>
2018-08-04 09:55:33 -0400

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Sara Burger", true);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Jose Burger", true);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Big Burger", true);
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burger", function (res) {
+      cb(res);
+    });
+  },
+
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burger", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (objColVals, burgerID, cb) {
+    orm.updateOne("burger", objColVals, burgerID, function (res) {
+      cb(res);
+    });
+  },
+};
+
+module.exports = burger;
